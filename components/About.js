a21facdb0d0c6057baf7b0c58fbe6d8f4756cf1a
Natalija Germek <n.germek@icloud.com>
2022-06-16 21:03:35 -0700

    added image and begining content to DevProfiles component, updated Container in Header

--- components/About.js
+++ components/About.js
@@ -1,1 +1,44 @@
-'use strict';
+import React from 'react';
+import { Component } from 'react';
+import Accordion from 'react-bootstrap/Accordion';
+import natalija from './natalija.jpeg';
+
+class DevProfiles extends Component {
+	render() {
+		return (
+			<Accordion>
+				<Accordion.Item eventKey="0">
+					<Accordion.Header>Natalija Germek</Accordion.Header>
+					<Accordion.Body id="natalijaColors">
+						<div className="photos">
+							<img src={natalija} alt="" height={150} />
+						</div>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+						labore
+					</Accordion.Body>
+				</Accordion.Item>
+				<Accordion.Item eventKey="1">
+					<Accordion.Header>Accordion Item #2</Accordion.Header>
+					<Accordion.Body>
+						{/* <div className="photos">
+        <img src={} alt="" height={200} /> 
+        </div> */}
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+						labore
+					</Accordion.Body>
+				</Accordion.Item>
+				<Accordion.Item eventKey="2">
+					<Accordion.Header>Accordion Item #3</Accordion.Header>
+					<Accordion.Body>
+						{/* <div className="photos">
+        <img src={} alt="" height={200} /> 
+        </div> */}
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+						labore
+					</Accordion.Body>
+				</Accordion.Item>
+			</Accordion>
+		);
+	}
+}
+export default DevProfiles;
--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
+import { Navbar, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Logout from './auth/Logout';
 import Profile from './auth/Profile';
