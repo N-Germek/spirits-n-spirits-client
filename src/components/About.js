a09b08f6ec362acb3425b2209509e9326aa22d4e
Natalija Germek <n.germek@icloud.com>
2022-06-23 21:11:46 -0700

    testing styling

--- src/components/About.js
+++ src/components/About.js
@@ -12,9 +12,9 @@ class About extends Component {
 					<Accordion.Header>Natalija Germek</Accordion.Header>
 					<Accordion.Body id="natalijaColors">
 						<div className="photos">
-							<img src={natalija} alt="" height={150} />
+							<img src={natalija} alt="" height={130} />
 						</div>
-						I'm a software developer with a background in the banking industry. For the last sixteen years I have worked almost every position in the banking center and in the market from auditing, sales, managing, and mentoring. I love to be in the great outdoors as much as possible and travel frequently. My interest in software development stemmed from a youth curiosity when I completed my first Cysco Networking class then developed as I got older and the problems of everyday life became more complex. 
+						I'm a software developer with a background in the banking industry. For the last sixteen years I have worked almost every position in the banking center and in the market from auditing, sales, managing, and mentoring. I love to be in the great outdoors as much as possible and travel frequently. My interest in software development stemmed from a youth curiosity when I completed my first Cysco Networking class then developed as I got older and the problems of everyday life became more complex. My passion is in finding technological solutions in everyday problems as efficiently as possible. 
 					</Accordion.Body>
 				</Accordion.Item>
 				<Accordion.Item eventKey="1">
