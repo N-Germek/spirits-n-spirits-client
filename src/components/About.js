0b02f59e8097baae621705e2a7372efa39cc87d1
Natalija Germek <n.germek@icloud.com>
2022-06-23 20:37:10 -0700

    working3

--- src/components/About.js
+++ src/components/About.js
@@ -14,13 +14,12 @@ class About extends Component {
 						<div className="photos">
 							<img src={natalija} alt="" height={150} />
 						</div>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-						labore
+						I'm a software developer with a background in the banking industry. For the last sixteen years I have worked almost every position in the banking center and in the market from auditing, sales, managing, and mentoring. I love to be in the great outdoors as much as possible and travel frequently. My interest in software development stemmed from a youth curiosity when I completed my first Cysco Networking class then developed as I got older and the problems of everyday life became more complex. 
 					</Accordion.Body>
 				</Accordion.Item>
 				<Accordion.Item eventKey="1">
-					<Accordion.Header>Accordion Item #2</Accordion.Header>
-					<Accordion.Body>
+					<Accordion.Header>Zayah Lang</Accordion.Header>
+					<Accordion.Body id="zayahColors">
 						<div className="photos">
         <img src={zayah} alt="" height={200} />
         </div>
@@ -28,8 +27,8 @@ class About extends Component {
 					</Accordion.Body>
 				</Accordion.Item>
 				<Accordion.Item eventKey="2">
-					<Accordion.Header>Accordion Item #3</Accordion.Header>
-					<Accordion.Body>
+					<Accordion.Header>Derek Douglas</Accordion.Header>
+					<Accordion.Body id="derekColors">
 						{/* <div className="photos">
         <img src={} alt="" height={200} />
         </div> */}
