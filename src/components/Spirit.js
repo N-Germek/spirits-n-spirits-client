78535e9782b72ea1b978e65ad282e07343a39c39
Derek Douglas <derek.j.douglas13@gmail.com>
2022-06-23 22:36:17 -0500

    adjusts navbar styling

--- src/components/Spirit.js
+++ src/components/Spirit.js
@@ -5,9 +5,7 @@ import Drink from './Drink.js';
 import Row from 'react-bootstrap/Row';
 import { withAuth0 } from "@auth0/auth0-react";
 import "../App.css"
-// import ButtonGroup from 'react-bootstrap/ButtonGroup';
-// import ToggleButton from 'react-bootstrap/ToggleButton';
-
+import Container from 'react-bootstrap/Container';
 
 // display image left of description for card
 class Spirit extends React.Component {
@@ -15,37 +13,14 @@ class Spirit extends React.Component {
 
   render() {
 
-    // const radios = [
-    //   { name: 'Alcoholic', value: 'alcoholic' },
-    //   { name: 'Non-Alcoholic', value: 'non-alcoholic' },
-    // ];
-
     return (
       <>
       {this.props.auth0.isAuthenticated &&
       <>
+      <Container className="centerButtons">
       <Button className="spiritButton" variant="outline-dark" onClick={this.props.drinkPreference}>{this.props.drinkTarget}</Button>
-      {/* <ButtonGroup
-
-        }}>
-
-        {radios.map((radio, idx) => (
-          <ToggleButton
-            key={idx}
-            id={`radio-${idx}`}
-            type="radio"
-            variant={"outline-dark"}
-            name="radio"
-            value={radio.value}
-            checked={radio.value}
-            onChange={(e) => {
-          this.props.drinkPreference(e.currentTarget.value);
-          >
-            {radio.name}
-          </ToggleButton>
-        ))}
-      </ButtonGroup> */}
       <Button className="spiritButton" variant="outline-dark" onClick={this.props.dailyReading}>Draw Card</Button>
+      </Container>
       <Row xs={1} sm={2} md={3} lg={4}>
         <Tarot draw={this.props.draw} />
       </Row>
