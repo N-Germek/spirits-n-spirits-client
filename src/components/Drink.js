8034ef7ae9ec5e308556d9a20dd1b23837998422
Aiden <hyena.shenanigans@gmail.com>
2022-06-23 20:58:05 -0700

    Finishes ingredient listing

--- src/components/Drink.js
+++ src/components/Drink.js
@@ -1,9 +1,38 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 // display image left of description for card
 class Drink extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ingredientArray: [],
+      measurementArray: []
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.draw !== this.props.draw) {
+           this.loadText();
+    }
+}
+  loadText = () => {
+    console.log('component  mounting');
+    console.log(this.props);
+    if (this.props.draw) {
+      this.setState({ ingredientArray: this.stringFilter('strIngredient'), measurementArray: this.stringFilter('strMeasure') });
+    }
+  }
+  stringFilter = (string) => {
+    let returnArray = [];
+    let objects = Object.entries(this.props.draw.drinkChosen).filter(([key]) => key.includes(string)).filter(([key]) => this.props.draw.drinkChosen[key] != null);
+    objects.forEach(obj => returnArray.push(obj[1]));
+    console.log(returnArray, 'return array');
+    return returnArray;
+  }
+  
   render() {
     return (
       <>
@@ -25,7 +54,11 @@ class Drink extends React.Component {
 
               <Card>
                 <Card.Text>
-                  Instructions: {`${this.props.draw.drinkChosen.strInstructions}`}
+                  <ListGroup>
+                  {
+                    this.state.ingredientArray.map((ingredient, index) => <ListGroup.Item key={index}>{this.state.measurementArray[index]} {ingredient}</ListGroup.Item> )
+                  }
+                  </ListGroup>
                 </Card.Text>
               </Card>
             </CardGroup>
